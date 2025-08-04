f7fe6439710ee27c62938eab7eeabc2a682f045d
luAr26101 <raul.cosmin.26@icloud.com>
2025-08-04 07:44:50 +0300

    Initial commit.

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const TIMEOUT_SEC = 20;
+export const RESULTS_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+export const API_KEY = '772e0192-60a3-48cc-b947-20b074780b07';
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
