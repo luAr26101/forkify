765cbdf91d1b5bf857b6bacd0ff8fc9ddae127b9
luAr26101 <raul.cosmin.26@icloud.com>
2025-08-05 08:15:01 +0300

    Test over.

--- src/js/controller.js
+++ src/js/controller.js
@@ -112,9 +112,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application!!!');
-};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -123,7 +120,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
   // model.loadSearchResults('pizza');
 };
 
